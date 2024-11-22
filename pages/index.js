21187c7d7195e39ed2f6c21f144bb15223de4a48
Caio Pellegrini <88295864+caio-pellegrini@users.noreply.github.com>
2024-11-22 21:48:45 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Vocês são os melhores pais do mundo! ❤️</h1>
+  return <h1>Vocês são os melhores pais do mundo! ❤️</h1>;
+}
+
+function teste() {
+  console.log('teste')
 }
 
 export default Home;
