4ca3262cedee3b87df5d77abe1134b00714f8ab1
Caio Pellegrini <88295864+caio-pellegrini@users.noreply.github.com>
2024-12-12 17:10:51 +0000

    adiciona objetivo do site na pagina 'em contrucao'

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Vocês são os melhores pais do mundo! ❤️</h1>;
+  return <h1>Bem-vindo ao RoboTab, um portal brasileiro onde pessoas interessadas por robótica educacional e pelas competições FLL, OBR e FRC se reunem para se ajudar mutuamente. Feito para estudantes, professores e entusiastas centrados e dedicados, dispostos a compartilhar o que sabem para o benefício da comunidade. Este é um projeto OpenSource que está em construção! Você pode contribuir.</h1>;
 }
 
 function teste() {
