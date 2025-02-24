082bb171b21a7b4ef073719bc07b9d442a86ec36
Caio Pellegrini <88295864+caio-pellegrini@users.noreply.github.com>
2025-02-24 01:48:53 +0000

    add `jsconfig.json` file

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 function Home() {
-  return <h1>Bem-vindo ao RoboTab, um portal brasileiro onde pessoas interessadas por robótica educacional e pelas competições FLL, OBR e FRC se reunem para se ajudar mutuamente. Feito para estudantes, professores e entusiastas centrados e dedicados, dispostos a compartilhar o que sabem para o benefício da comunidade. Este é um projeto OpenSource que está em construção! Você pode contribuir.</h1>;
+  return (
+    <h1>
+      Bem-vindo ao RoboTab, um portal brasileiro onde pessoas interessadas por
+      robótica educacional e pelas competições FLL, OBR e FRC se reunem para se
+      ajudar mutuamente. Feito para estudantes, professores e entusiastas
+      centrados e dedicados, dispostos a compartilhar o que sabem para o
+      benefício da comunidade. Este é um projeto OpenSource que está em
+      construção! Você pode contribuir.
+    </h1>
+  );
 }
 
 function teste() {
-  console.log('teste')
+  console.log("teste");
 }
 
 export default Home;
